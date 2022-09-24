044ac1e3b4febe9656b790279a994642f375a23a
kootstra.rene <kootstra.rene@gmail.com>
2022-09-24 15:36:48 +0200

    updated svg tutorials to latest changes

--- user/svg/clock.js
+++ user/svg/clock.js
@@ -1,21 +1,24 @@
 mdlr('[html]svg-clock', m => {
 
-    m.html`
-    <svg xmlns="http://www.w3.org/2000/svg" width="400px" height="400px" viewBox="-50 -50 100 100">
-      <circle xmlns="http://www.w3.org/2000/svg" class="dialplate" r="48"></circle>
+  m.html`
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100">
+      <circle class="dialplate" r="48" />
+
       {#each minute in [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]}
-        <line xmlns="http://www.w3.org/2000/svg" class="major" y1="35" y2="45" transform="rotate({30 * minute})"></line>
+        <line class="major" y1="35" y2="45" transform="rotate({6 * minute})" />
+
         {#each offset in [1, 2, 3, 4]}
-          <line xmlns="http://www.w3.org/2000/svg" class="minor" y1="42" y2="45" transform="rotate({6 * (minute + offset)})"></line>
+          <line class="minor" y1="42" y2="45" transform="rotate({6 * (minute + offset)})" />
         {/each}
       {/each}
-      <line xmlns="http://www.w3.org/2000/svg" class="hour" y1="2" y2="-20" transform="rotate({30 * hours + minutes / 2})"></line>
-      <line xmlns="http://www.w3.org/2000/svg" class="minute" y1="4" y2="-30" transform="rotate({6 * minutes + seconds / 10})"></line>
-      <line xmlns="http://www.w3.org/2000/svg" class="second" y1="10" y2="-38" transform="rotate({6 * seconds})"></line>
-    </svg>`;
-  
 
-    m.css`:root {
+      <line class="hour" y1="2" y2="-20" transform="rotate({30 * hours + minutes / 2})" />
+      <line class="minute" y1="4" y2="-30" transform="rotate({6 * minutes + seconds / 10})" />
+      <line class="second" y1="10" y2="-38" transform="rotate({6 * seconds})" />
+    </svg>`;
+
+
+  m.css`:root {
       position: absolute;
       left: 0;
       top: 0;
@@ -54,26 +57,25 @@ mdlr('[html]svg-clock', m => {
       width: 100%;
       height: 100%;
     }`;
-  
-    return class {
-      hours = 0;
-      minutes = 0;
-      seconds = 0;
-  
-      #updateTime() {
-        let time = new Date();
-        this.hours = time.getHours();
-        this.minutes = time.getMinutes();
-        this.seconds = time.getSeconds();
 
-        m.render(this);
-      }
+  return class {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
 
-      connected() {
-        setInterval(this.#updateTime.bind(this), 250);
-        this.#updateTime();
-      }
+    #updateTime() {
+      let time = new Date();
+      this.hours = time.getHours();
+      this.minutes = time.getMinutes();
+      this.seconds = time.getSeconds();
+
+      m.render(this);
     }
-  
-  })
-  
+
+    connected() {
+      setInterval(this.#updateTime.bind(this), 250);
+      this.#updateTime();
+    }
+  }
+
+})
