14cf054cade6a12751c2789a836e0d9a5a93f924
kootstra.rene <kootstra.rene@gmail.com>
2022-09-28 17:12:04 +0200

    updated scatterplot with grid

--- user/scatterplot.js
+++ user/scatterplot.js
@@ -5,26 +5,30 @@ mdlr('[html]scatterplot', m => {
 
   m.html`
     <div class="chart">
-    <h2>Anscombe's quartet</h2>
-
-    {#each set in dataset}
-      <m-scatterplot-graph points={set.data}></m-scatterplot-graph>
-    {/each}
+      <h2>Anscombe's quartet</h2>
+      {#each set in dataset}
+        <m-scatterplot-graph points={set.data}></m-scatterplot-graph>
+      {/each}
     </div>`;
 
   m.css`
+    :root {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);      
+    }
+
     .chart {
       background-color: #ccc;
       width: 100%;
       max-width: 640px;
-      height: calc(100% - 4em);
-      min-height: 280px;
-      max-height: 480px;
       margin: 0 auto;
+      padding: 1em;
     }`;
 
   return class {
-    dataset = ['a', 'b', 'c', 'd'].map(id => ({ id, data: data[id] }));
+    dataset = ['a', 'b', 'c', 'd'].map(id => ({ data: data[id] }));
   }
 
 })
@@ -33,8 +37,26 @@ mdlr('[html]scatterplot-graph', m => {
 
   m.html`
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {width} {height}">
+    <g class="axis y-axis">
+		{#each tick in yTicks}
+			<g class="tick tick-{tick}" transform="translate(0, {scaleY(tick)})">
+				<line x1="{padding.left}" x2="{scaleX(22)}"/>
+				<text x="{padding.left - 8}" y="+4">{tick}</text>
+			</g>
+		{/each}
+	  </g>
+
+    <g class="axis x-axis">
+		{#each tick in xTicks}
+			<g class="tick" transform="translate({scaleX(tick)},0)">
+				<line y1="{scaleY(0)}" y2="{scaleY(13)}"/>
+				<text y="{height - padding.bottom + 16}">{tick}</text>
+			</g>
+		{/each}
+	  </g>
+
     {#each point in points}
-		  <circle cx={scaleX(point.x)} cy={scaleY(point.y)} r="0.5"/>
+		  <circle cx={scaleX(point.x)} cy={scaleY(point.y)} r="5"/>
 	  {/each}
     </svg>`;
 
@@ -43,6 +65,24 @@ mdlr('[html]scatterplot-graph', m => {
       display: inline-block;
       height: 50%;
       width: 50%;
+    }
+
+    .tick line {
+      stroke: #999;
+      stroke-dasharray: 2;
+    }
+
+    .x-axis text {
+      text-anchor: middle;
+    }
+  
+    .y-axis text {
+      text-anchor: end;
+    }
+
+    text {
+      font-size: 12px;
+      fill: #999;
     }
 
     svg {
@@ -54,20 +94,49 @@ mdlr('[html]scatterplot-graph', m => {
       fill: orange;
       fill-opacity: 0.6;
       stroke: rgba(0, 0, 0, 0.5);
-      stroke-width: 0.05;
+      stroke-width: 0.5;
     }`;
 
+  function linear() {
+    const instance = {
+      dMin: 0,
+      dMax: 0,
+      rMin: 0,
+      rMax: 0,
+      domain: (min, max) => {
+        instance.dMin = min;
+        instance.dMax = max;
+        return instance;
+      },
+      range: (min, max) => {
+        instance.rMin = min;
+        instance.rMax = max;
+        return (v) => {
+          const { dMin, dMax, rMin, rMax } = instance;
+          return rMin + (((v - dMin) / (dMax - dMin)) * (rMax - rMin));
+        };
+      },
+    };
+
+    return instance;
+  }
+
   return class {
-    width = 20;
-    height = 12;
+    width = 320;
+    height = 240;
     points = [];
+    xTicks = [0, 4, 8, 12, 16, 20];
+    yTicks = [0, 4, 8, 12];
+    padding = { top: 20, right: 40, bottom: 40, left: 25 };
+    xScale = linear().domain(0, 20).range(this.padding.left, this.width - this.padding.right);
+    yScale = linear().domain(0, 12).range(this.height - this.padding.bottom, this.padding.top);
 
     scaleX(x) {
-      return x;
+      return (this.xScale(x)).toFixed(1);
     }
 
     scaleY(y) {
-      return this.height - y;
+      return (this.yScale(y)).toFixed(1);
     }
   };
 
