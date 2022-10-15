b9607692488462a9c8345b354f3a1037af60d54c
kootstra.rene <kootstra.rene@gmail.com>
2022-10-15 23:18:40 +0200

    github pages of development blog

--- pages.sh
+++ pages.sh
@@ -1,5 +1,5 @@
-curl -s --insecure 'https://localhost:8443/bundler/html?unit=\[html\]mdlr-blog' | gunzip | sed 's/^ *//g' > docs/blog/index.html
+curl -s --insecure 'https://localhost:8443/bundler/html?unit=\[html\]mdlr-blog' | gunzip | sed 's/^ *//g' > docs/index.html
 
-curl -s --insecure 'https://localhost:8443/bundler/html?unit=\[html\]tutorial-svg-clock' | gunzip | sed 's/^ *//g' > docs/blog/resources/svg-clock.html
+curl -s --insecure 'https://localhost:8443/bundler/html?unit=\[html\]tutorial-svg-clock' | gunzip | sed 's/^ *//g' > docs/resources/svg-clock.html
 
-curl -s --insecure 'https://localhost:8443/bundler/html?unit=\[html\]tutorial-firewatch' | gunzip | sed 's/^ *//g' > docs/blog/resources/firewatch.html
+curl -s --insecure 'https://localhost:8443/bundler/html?unit=\[html\]tutorial-firewatch' | gunzip | sed 's/^ *//g' > docs/resources/firewatch.html
