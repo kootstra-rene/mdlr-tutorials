1b450a4e8612c85dad8b85da0ee923137dcbec3b
kootstra.rene <kootstra.rene@gmail.com>
2022-11-07 10:58:09 +0100

    fix link styling

--- user/blog/blog-post-sections.js
+++ user/blog/blog-post-sections.js
@@ -2,7 +2,11 @@ mdlr('[html]blog-post-sections', m => {
 
   m.require('[html]blog-post-section-text');
 
-  m.html`<a href="https://github.com/kootstra-rene/mdlr-tutorials/tree/main/docs/{post?.slug}">edit</a><blog-post-section-text{=} />`;
+  m.html`
+  {#if !!post?.body}
+  <a href="https://github.com/kootstra-rene/mdlr-tutorials/tree/main/docs/{post?.slug}">edit</a>
+  {/if}
+  <blog-post-section-text{=} />`;
 
   m.css`
   * {
@@ -16,7 +20,7 @@ mdlr('[html]blog-post-sections', m => {
     display: block;
     box-shadow: 0 0 black;
   }
-  a {
+  :root > a {
     position:absolute;
     right:0.75em;
     top:0em;
